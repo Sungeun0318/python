90b688a01bc96e4976468a2b5f8a3b451c827d94
Sungeun0318 <kimsungeun0318@naver.com>
2026-04-20 09:53:53 +0900

    0417

---
+++ day03/exam.1.py
@@ -0,0 +1,21 @@
+key_list = ["name", "hp", "mp", "level"]
+value_list = ["기사", 200, 30, 5]
+character = {}
+
+for i in range(len(key_list)):
+    character[key_list[i]] = value_list[i]
+
+print(character)
+
+
+
+
+
+# limit = 10000
+# i = 1
+
+# while sum_value <= limit:
+#     sum_value += i
+#     i += 1
+
+# print("{}를 더할 때 {}을 넘으며 그때의 값은 {} 입니다.".format(i-1, limit, sum_value))
