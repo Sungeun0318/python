9808e57de26f5993a3d70c45d51fb1a62abc56f3
Sungeun0318 <kimsungeun0318@naver.com>
2026-05-12 10:26:10 +0900

    0512

--- day18/T8-02.py
+++ day18/T8-02.py
@@ -12,8 +12,9 @@ if __name__ == "__main__" :
 # 4. REST 정의하기
 # REST : 자원 주고 받는 상태 구조
 # REST API : 자원 주고 받는 상태 구조조 # REST API : HTTP로 REST 구현
+# 자동으로 JSON 타입으로 응답한다. vs # @ResponseBoby(@ㄱRestController)
 # @GetMapping("/URL") vs @app.get("/URL")
 
-@app.get("/")
+@app.get("/") # HTTP GET 방식으로 매핑한다 # 주소 정의
 async def index() :
     return "안녕 파이썬웹"
